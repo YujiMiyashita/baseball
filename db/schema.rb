e9af7781ac0c3558ca825da7a6af2ae12154d5fc
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-10-31 22:04:20 +0900

    友達つながり機能１

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031061802) do
+ActiveRecord::Schema.define(version: 20161031085808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,11 +33,26 @@ ActiveRecord::Schema.define(version: 20161031061802) do
     t.text     "content"
     t.string   "nick_name"
     t.string   "back_image"
+    t.integer  "team_id"
+    t.integer  "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_index "profiles", ["player_id"], name: "index_profiles_on_player_id", using: :btree
+  add_index "profiles", ["team_id"], name: "index_profiles_on_team_id", using: :btree
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
+  create_table "tribes", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tribes", ["followed_id"], name: "index_tribes_on_followed_id", using: :btree
+  add_index "tribes", ["follower_id", "followed_id"], name: "index_tribes_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "tribes", ["follower_id"], name: "index_tribes_on_follower_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",           null: false
