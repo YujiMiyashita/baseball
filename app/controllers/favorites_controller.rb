7e6ab97f44977d894014386ee086f59599b75b3e
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-10-31 17:08:15 +0900

    コントローラ修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,8 +5,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = current_user.favorites.find_by(to_user_id: params[:favorite][:to_user_id])
-    @favorite.destroy
+    @favorite = current_user.favorites.find_by(to_user_id: params[:favorite][:to_user_id])destroy
     redirect_to users_url, notice: "#{@favorite.to_user.user_name}さんをお気に入り解除しました"
   end
 end
