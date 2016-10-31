b734298dd9cd90a061391d9f8fe9bc869fab39d6
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-10-31 23:32:58 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
    password: ENV["SENDGRID_PASSWORD"],
    domain: "heroku.com",
    address: "smtp.sendgrid.net",
-   port: 587,
+   port: 465,
    authentication: :plain,
    enable_starttls_auto: true
   }
