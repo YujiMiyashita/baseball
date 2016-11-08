41a6eb4e4b36f411bec1e1ee176173ba83b07999
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-11-09 01:38:58 +0900

    観戦日記

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108084122) do
+ActiveRecord::Schema.define(version: 20161108152744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20161108084122) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "blogs", ["user_id"], name: "index_blogs_on_user_id", using: :btree
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "from_user_id"
@@ -129,6 +140,7 @@ ActiveRecord::Schema.define(version: 20161108084122) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "seats", "ballparks"
   add_foreign_key "tickets", "ballparks"
