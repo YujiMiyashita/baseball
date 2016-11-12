840a4cf6e141451061847870b0c3dc1750bb78c0
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-11-12 13:30:16 +0900

    タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Baseball
     #日本語化
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
