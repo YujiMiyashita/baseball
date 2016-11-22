e55b0dddd45d50e5f3ad885f9d35ab75b7dc8597
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-11-22 16:33:04 +0900

    期限内のチケットのみ一覧表示

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,11 @@ class Ticket < ActiveRecord::Base
   belongs_to :ballpark
   belongs_to :visitor, class_name: 'Team'
   belongs_to :home, class_name: 'Team'
+
+  scope :limit_bitween, -> current_time {
+    where('post_start_at <= ?', current_time).where('post_end_at >= ?', current_time)
+  }
+
+  scope :valid, -> { where(status: true) }
+  scope :disabled, -> { where(status: false) }
 end
