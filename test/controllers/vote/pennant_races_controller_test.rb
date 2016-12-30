e47c919f8de3efed2b3caef8e25940a45e8a7651
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-12-30 23:04:01 +0900

    トレードモデルを削除

--- test/controllers/vote/pennant_races_controller_test.rb
+++ test/controllers/vote/pennant_races_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TradeTest < ActiveSupport::TestCase
+class Vote::PennantRacesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
