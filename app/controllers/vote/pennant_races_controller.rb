dd80fea266328c03747703c3b4c49fcdc1d5c548
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2017-01-01 22:45:01 +0900

    index, new, createアクション実装

--- app/controllers/vote/pennant_races_controller.rb
+++ app/controllers/vote/pennant_races_controller.rb
@@ -1,21 +1,27 @@
 class Vote::PennantRacesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_pennant_race
+  before_action :set_pennant_race, only: [:edit]
 
   def index
-    @pennant_races = PennantRace.index_all
+    @central_anticipates = PennantRace.index_all.this_season(Date.today.year).central
+    @pacific_anticipates = PennantRace.index_all.this_season(Date.today.year).pacific
   end
 
   def new
-    @pennant_race = PennantRace.new
-
+    @central_teams = Team.select(:id, :name).central
+    @pacific_teams = Team.select(:id, :name).pacific
   end
 
   def edit
   end
 
   def create
-    @pennant_race = current_user.pennant_race.build
+    @pennant_race = current_user.pennant_races.build(pennant_race_params)
+    if @pennant_race.save
+      redirect_to vote_pennant_races_path
+    else
+      render :new
+    end
   end
 
   private
@@ -25,6 +31,6 @@ class Vote::PennantRacesController < ApplicationController
   end
 
   def pennant_race_params
-    params.require(:pennant_race).permit(:league, :user_id, :first_team_id, :second_team_id, :third_team_id, :fourth_team_id, :fifth_team_id, :sixth_team_id)
+    params.require(:pennant_race).permit(:season, :league, :user_id, :first_team_id, :second_team_id, :third_team_id, :fourth_team_id, :fifth_team_id, :sixth_team_id)
   end
 end
