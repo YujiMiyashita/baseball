1e43732507bdc6720eeb72a1cd8c95eb08fdbfc7
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2017-01-01 22:44:37 +0900

    アソシエーション定義

--- app/models/pennant_race.rb
+++ app/models/pennant_race.rb
@@ -1,14 +1,21 @@
 class PennantRace < ActiveRecord::Base
   belongs_to :user
 
+  belongs_to :first_team,  class_name: 'Team'
+  belongs_to :second_team, class_name: 'Team'
+  belongs_to :third_team,  class_name: 'Team'
+  belongs_to :fourth_team, class_name: 'Team'
+  belongs_to :fifth_team,  class_name: 'Team'
+  belongs_to :sixth_team,  class_name: 'Team'
+
   scope :index_all, -> {
-    select(:id, :league, :created_at,
+    select(:id, :league, :created_at, :user_id,
            :first_team_id, :second_team_id, :third_team_id, :fourth_team_id, :fifth_team_id, :sixth_team_id)
     .order(created_at: :asc)
   }
 
   scope :this_season, -> current_season {
-    
+    where(season: current_season)
   }
 
   enum league: { central: true, pacific: false }
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ActiveRecord::Base
   belongs_to :team
   has_many :profile
+
+  enum position: { picher: 0, catcher: 1, infielder: 2, outfielder: 3 }
 end
