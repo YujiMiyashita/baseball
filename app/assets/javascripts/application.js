64237be30b0dcec73fb0ffa5430e6f0d9d73302f
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2017-01-04 18:37:55 +0900

    turbolinks抹殺

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require twitter/bootstrap
 //= require jquery.autosize
 //= require moment
